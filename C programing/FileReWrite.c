e6e97158e151d88887aa986253530486e88b13fa
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-09-16 11:39:30 +0530

    Update FileReWrite.c

--- C programing/FileReWrite.c
+++ C programing/FileReWrite.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp1;
+    char name[10];
+    fp1=fopen("aa1.txt","w");
+    //fp2=fopen("aa2.txt","w");
+    printf("Enter the Name");
+    scanf("%s",name);
+    fwrite(name,sizeof(10),1,fp1);
+
+    fp1=fopen("aa1.txt","r");
+     fread(name,sizeof(10),1,fp1);
+     printf("name=%s",name);
+
+    fclose(fp1);
+    //fclose(fp2);
+    return 0;
+
+}
 
