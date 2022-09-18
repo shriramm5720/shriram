2356be8fe5e32f7c0fdaf83e3415f1ada6b41d3e
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-09-18 12:23:16 +0530

    Create Stack.java

---
+++ JAVA/Stack.java
@@ -0,0 +1,22 @@
+package Java.arry;
+
+import java.util.Stack;
+
+
+
+public class StackMethod {
+
+	public static void main(String[] args) {
+		Stack<Integer> demo=new Stack<>();
+		
+	
+
+		if(demo.isEmpty())
+			System.out.println("Stack is empty");
+		else
+			System.out.println("Not Empty");
+		//demo.peek();
+		
+	}
+
+}
