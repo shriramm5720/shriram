5684df0a31eb178b0eaf69c3f7dadc9b48aa80f8
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-09-19 18:02:35 +0530

    Create compareTo()Method.java

---
+++ JAVA/compareTo()Method.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Collections;
+
+
+class Main{
+    static int  result;
+public static void main(String []args)
+{
+    List<Integer> demo=new LinkedList<>();
+    demo.add(5);
+    demo.add(1);
+    demo.add(4);
+    demo.add(7);
+    demo.add(2);
+    demo.add(3);
+    demo.add(6);
+    
+new Main().printList(demo);
+System.out.println("*******************************");
+demo.sort(null);
+new Main().printList(demo);
+System.out.println("*******************************");
+Collections.reverse(demo);
+new Main().printList(demo);
+}
+void printList(List<Integer> list){
+    Iterator<Integer> iterator=list.iterator();
+    while(iterator.hasNext())
+    {
+        
+    
+            System.out.println(iterator.next().compareTo(7));// print 0 if condition satisfy otherwise -1 print
+       
+   
+        }
+    }
+}
