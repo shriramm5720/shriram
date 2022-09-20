dad2ad23d021d457f999b935954ff9caf84d0956
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-09-20 10:51:35 +0530

    Create CallByValue.cpp

---
+++ CPP/CallByValue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void display(int a)
+{
+    cout<<"a="<<a<<endl;
+    a=100;
+
+}
+int main()
+{
+    int x=10;
+    cout<<"x="<<x<<endl;
+    display(x);
+    cout<<"x="<<x<<endl;
+}
