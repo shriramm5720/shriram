5354fba3b8309dcda3421cfa97ae02ad871e4aea
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-09-27 10:41:50 +0530

    Create DigitGet.cpp

---
+++ CPP/ExceptionHandling/DigitGet.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    int i;
+    cout<<"Enter the String\n";
+    cin>>s;
+
+    try
+    {
+       i=0;
+       while(s[i]!='\0')
+             {
+                    if((s[i]>='0')&&(s[i]<='9'))
+                      {
+                          throw("fh");
+                      }else
+                      {
+                          cout<<s[i];
+                      }
+                 i++;
+             }
+    }catch(int a)
+    {
+        cout<<"Integer TAKE"<<endl;
+    catch(char  *s)
+    {
+        cout<<"String Take"<<endl;
+    }catch(...)
+    {
+        cout<<"\n error"<<endl;
+    }
+    cout<<"program End";
+}
+
