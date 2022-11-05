a0344eccf878ad666a90bca90eff9fc0974bbbb0
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-11-05 10:43:56 +0530

    Create div_two_num_without_using_*.java

---
+++ JAVA/Loop/div_two_num_without_using_*.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GCD {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b, gcd;
+		System.out.println("Enter the two number");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int ans = 0;
+		for (int i = 1; i <= b; i++) {
+		      if(a>=b)
+		      {
+		    	  a-=b;
+		    	  ans++;
+		      }
+		}
+		System.out.println(ans);    
+	}
+
+}
