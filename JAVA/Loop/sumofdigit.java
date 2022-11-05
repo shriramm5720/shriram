ab6a3af1986bef0aca46e6de6f0360655ee3e403
shriramm5720 <112922615+shriramm5720@users.noreply.github.com>
2022-11-05 09:58:25 +0530

    Create sumofdigit.java

---
+++ JAVA/Loop/sumofdigit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumofDigit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt(); 
+	   
+		int rem;
+	  int sum=0;
+       while(n>0)
+       {
+    	   rem=n%10;
+    	   sum=sum+rem;
+    	  
+    	   n=n/10;
+       }
+       System.out.println(sum);
+	}
+
+}
